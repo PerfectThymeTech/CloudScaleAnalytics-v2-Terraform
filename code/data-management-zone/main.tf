03011851b499740792b8da9aa31fda6bb08c32d1
Marvin Buss <marvin.buss@gmail.com>
2023-02-11 17:13:51 +0100

    Add main terraform

---
+++ code/data-management-zone/main.tf
@@ -0,0 +1,65 @@
+terraform {
+    required_version = ">=0.12"
+
+    required_providers {
+      azurerm = {
+        source = "hashicorp/azurerm"
+        version = "3.43.0"
+      }
+    }
+
+    backend "azurerm" {
+      environment = "public"
+      resource_group_name = "terraform"
+      storage_account_name = "terraformptt001"
+      container_name = "tfstate"
+      key = "dev.terraform.tfstate"
+      use_oidc = true
+    }
+}
+
+provider "azurerm" {
+  disable_correlation_request_id = false
+  environment = "public"
+  skip_provider_registration = false
+  storage_use_azuread = true
+  use_oidc = true
+  features {
+    key_vault {
+      recover_soft_deleted_key_vaults = true
+      recover_soft_deleted_certificates = true
+      recover_soft_deleted_keys = true
+      recover_soft_deleted_secrets = true
+    }
+    network {
+      relaxed_locking = true
+    }
+    resource_group {
+      prevent_deletion_if_contains_resources = true
+    }
+  }
+}
+
+resource "azurerm_resource_group" "governance_rg" {
+  name = "${local.prefix}-governance-rg"
+  location = var.location
+  tags = var.tags
+}
+
+resource "azurerm_resource_group" "container_rg" {
+  name = "${local.prefix}-container-rg"
+  location = var.location
+  tags = var.tags
+}
+
+resource "azurerm_resource_group" "consumption_rg" {
+  name = "${local.prefix}-consumption-rg"
+  location = var.location
+  tags = var.tags
+}
+
+resource "azurerm_resource_group" "automation_rg" {
+  name = "${local.prefix}-automation-rg"
+  location = var.location
+  tags = var.tags
+}
