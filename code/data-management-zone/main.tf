f1d1b6aaa339b9cadd680e4ba8d02e8ebe248b1f
Marvin Buss <marvin.buss@gmail.com>
2023-02-12 19:07:16 +0100

    Update key in main

--- code/data-management-zone/main.tf
+++ code/data-management-zone/main.tf
@@ -17,7 +17,7 @@ terraform {
       resource_group_name = "terraform"
       storage_account_name = "terraformptt001"
       container_name = "tfstate"
-      key = "terraform.tfstate"
+      key = "terraform.data-management-zone.tfstate"
       use_oidc = true
     }
 }
