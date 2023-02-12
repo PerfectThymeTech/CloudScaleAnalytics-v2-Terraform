5d04d496858ef7d9d5d0202a6ac63554ca80d0be
Marvin Buss <marvin.buss@gmail.com>
2023-02-12 17:21:30 +0100

    Add locals definition

--- code/data-management-zone/locals.tf
+++ code/data-management-zone/locals.tf
@@ -1,3 +1,25 @@
 locals {
   prefix = "${lower(var.prefix)}-${var.environment}"
+
+  eventhub_namespace = {
+    min_throughput = 1
+    max_throughput = 1
+    eventhub_notification = {
+      name = "notification"
+      message_retention = 3
+      partition_count = 1
+    }
+    eventhub_hook = {
+      name = "hook"
+      message_retention = 3
+      partition_count = 1
+    }
+  }
+
+  virtual_network = {
+    resource_group_name = split("/", var.vnet_id)[4]
+    name = split("/", var.vnet_id)[8]
+    address_space = azurerm_virtual_network.virtual_network.address_space
+    cidr_prefix_length = reverse(split("/", azurerm_virtual_network.virtual_network.address_space))[0]
+  }
 }
