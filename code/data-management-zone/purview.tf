18d8cbe017322c3a498e0a0c470a52b9ddfe310d
Marvin Buss <marvin.buss@gmail.com>
2023-02-12 11:50:44 +0100

    Add Purview Kafka config

--- code/data-management-zone/purview.tf
+++ code/data-management-zone/purview.tf
@@ -9,3 +9,40 @@ resource "azurerm_purview_account" "purview" {
   managed_resource_group_name = "${purview.name}-rg"
   public_network_enabled = false
 }
+
+resource "azapi_resource" "purviewKafkaConfigurationNotification" {
+  type = "Microsoft.Purview/accounts/kafkaConfigurations@2021-12-01"
+  name = "notification"
+  parent_id = azurerm_purview_account.purview.id
+
+  body = jsonencode({
+    properties = {
+      credentials: {
+        type: "SystemAssigned"
+      }
+      eventHubResourceId = 
+      eventHubType = "Notification"
+      eventStreamingState = "Enabled"
+      eventStreamingType = "Azure"
+    }
+  })
+}
+
+resource "azapi_resource" "purviewKafkaConfigurationHook" {
+  type = "Microsoft.Purview/accounts/kafkaConfigurations@2021-12-01"
+  name = "notification"
+  parent_id = azurerm_purview_account.purview.id
+
+  body = jsonencode({
+    properties = {
+      credentials: {
+        type: "SystemAssigned"
+      }
+      eventHubResourceId = 
+      eventHubType = "Hook"
+      eventStreamingState = "Enabled"
+      eventStreamingType = "Azure"
+      consumerGroup = "$Default"
+    }
+  })
+}
