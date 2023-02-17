82b60cfdc66c54660f97ccc6de492811bd5926a0
Marvin Buss <marvin.buss@gmail.com>
2023-02-17 17:55:25 +0100

    Add resource groups

--- code/data-landing-zone/main.tf
+++ code/data-landing-zone/main.tf
@@ -57,8 +57,32 @@ provider "azapi" {
 data "azurerm_client_config" "current" {
 }
 
+resource "azurerm_resource_group" "management_rg" {
+  name     = "${local.prefix}-mgmt-rg"
+  location = var.location
+  tags     = var.tags
+}
+
 resource "azurerm_resource_group" "storage_rg" {
-  name     = "${local.prefix}-governance-rg"
+  name     = "${local.prefix}-storage-rg"
+  location = var.location
+  tags     = var.tags
+}
+
+resource "azurerm_resource_group" "runtimes_rg" {
+  name     = "${local.prefix}-runtimes-rg"
+  location = var.location
+  tags     = var.tags
+}
+
+resource "azurerm_resource_group" "shared_app_aut_rg" {
+  name     = "${local.prefix}-shared-app-aut-rg"
+  location = var.location
+  tags     = var.tags
+}
+
+resource "azurerm_resource_group" "shared_app_exp_rg" {
+  name     = "${local.prefix}-shared-app-exp-rg"
   location = var.location
   tags     = var.tags
 }
