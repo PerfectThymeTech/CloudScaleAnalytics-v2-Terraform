4a93b41df608daa5cdedb231cd3fac799b77639d
Marvin Buss <marvin.buss@gmail.com>
2023-02-22 11:11:38 +0100

    Update data lake module with few settings

--- code/data-landing-zone/modules/datalake/outputs.tf
+++ code/data-landing-zone/modules/datalake/outputs.tf
@@ -1,3 +1,5 @@
 output "datalake_id" {
-  value = azurerm_storage_account.datalake.id
+  value       = azurerm_storage_account.datalake.id
+  description = "Specifies the resource ID of the datalake."
+  sensitive   = false
 }
--- code/data-landing-zone/modules/datalake/variables.tf
+++ code/data-landing-zone/modules/datalake/variables.tf
@@ -1,11 +1,13 @@
 variable "location" {
   description = "Specifies the location for all Azure resources."
   type        = string
+  sensitive   = false
 }
 
 variable "datalake_name" {
   description = "Specifies the name of the ADLS Gen2 account."
   type        = string
+  sensitive   = false
   validation {
     condition     = length(var.datalake_name) >= 2
     error_message = "Please specify a valid name."
@@ -15,21 +17,25 @@ variable "datalake_name" {
 variable "datalake_filesystem_names" {
   description = "Specifies the names of the ADLS Gen2 containers."
   type        = list(string)
+  sensitive   = false
 }
 
 variable "tags" {
   description = "Specifies the tags that you want to apply to all resources."
   type        = map(any)
+  sensitive   = false
 }
 
 variable "resource_group_name" {
   description = "Resource Group Name"
   type        = string
+  sensitive   = false
 }
 
 variable "subnet_id" {
   description = "Specifies the resource ID of the subnet used for the datalake."
   type        = string
+  sensitive   = false
   validation {
     condition     = length(split("/", var.vnet_id)) == 11
     error_message = "Please specify a valid resource ID."
@@ -39,6 +45,7 @@ variable "subnet_id" {
 variable "private_dns_zone_id_blob" {
   description = "Specifies the resource ID of the private DNS zone for Azure Storage blob endpoints."
   type        = string
+  sensitive   = false
   validation {
     condition     = var.private_dns_zone_id_blob == "" || (length(split("/", var.private_dns_zone_id_blob)) == 9 && endswith(var.private_dns_zone_id_blob, "privatelink.blob.core.windows.net"))
     error_message = "Please specify a valid resource ID for the private DNS Zone."
@@ -48,6 +55,7 @@ variable "private_dns_zone_id_blob" {
 variable "private_dns_zone_id_dfs" {
   description = "Specifies the resource ID of the private DNS zone for Azure Storage dfs endpoints."
   type        = string
+  sensitive   = false
   validation {
     condition     = var.private_dns_zone_id_dfs == "" || (length(split("/", var.private_dns_zone_id_dfs)) == 9 && endswith(var.private_dns_zone_id_dfs, "privatelink.dfs.core.windows.net"))
     error_message = "Please specify a valid resource ID for the private DNS Zone."
