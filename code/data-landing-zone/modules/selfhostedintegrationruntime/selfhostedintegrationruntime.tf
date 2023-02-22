7926be810b2e92449f3cb8bab0284ad81fd0be32
Marvin Buss <marvin.buss@gmail.com>
2023-02-22 11:12:25 +0100

    Add sharing of shir

--- code/data-landing-zone/modules/selfhostedintegrationruntime/selfhostedintegrationruntime.tf
+++ code/data-landing-zone/modules/selfhostedintegrationruntime/selfhostedintegrationruntime.tf
@@ -1,4 +1,9 @@
+data "azurerm_data_factory" "data_factory" {
+  name = local.data_factory.name
+  resource_group_name = local.data_factory.resource_group_name
+}
+
 resource "azurerm_data_factory_integration_runtime_self_hosted" "data_factory_shir" {
   name            = var.selfhostedintegrationruntime_name
-  data_factory_id = var.data_factory_id
+  data_factory_id = data.azurerm_data_factory.data_factory.id
 }
--- code/data-landing-zone/modules/selfhostedintegrationruntime/variables.tf
+++ code/data-landing-zone/modules/selfhostedintegrationruntime/variables.tf
@@ -1,11 +1,13 @@
 variable "location" {
   description = "Specifies the location for all Azure resources."
   type        = string
+  sensitive   = false
 }
 
 variable "selfhostedintegrationruntime_name" {
   description = "Specifies the name of the Self-hosted integration runtime."
   type        = string
+  sensitive   = false
   validation {
     condition     = length(var.selfhostedintegrationruntime_name) >= 2
     error_message = "Please specify a valid name."
@@ -15,16 +17,19 @@ variable "selfhostedintegrationruntime_name" {
 variable "tags" {
   description = "Specifies the tags that you want to apply to all resources."
   type        = map(any)
+  sensitive   = false
 }
 
 variable "resource_group_name" {
   description = "Resource Group Name"
   type        = string
+  sensitive   = false
 }
 
 variable "data_factory_id" {
   description = "Specifies the resource ID of the Azure Data Factory."
   type        = string
+  sensitive   = false
   validation {
     condition     = length(split("/", var.vnet_id)) == 9
     error_message = "Please specify a valid resource ID."
@@ -32,8 +37,9 @@ variable "data_factory_id" {
 }
 
 variable "subnet_id" {
-  description = "Specifies the resource ID of the subnet used for the datalake."
+  description = "Specifies the resource ID of the subnet used for the SHIR."
   type        = string
+  sensitive   = false
   validation {
     condition     = length(split("/", var.vnet_id)) == 11
     error_message = "Please specify a valid resource ID."
@@ -49,5 +55,11 @@ variable "admin_username" {
 variable "admin_password" {
   description = "Specifies the admin password of the VMMS of the Self-hosted integration runtime."
   type        = string
+  sensitive   = true
+}
+
+variable "shared_data_factory_ids" {
+  description = "Specifies the resource IDs of the Azure Data Factories with which the SHIR should be shared."
+  type        = list(string)
   sensitive   = true
 }
