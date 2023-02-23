d8d766eb3932d05a24361d43504db49850f202ff
Marvin Buss <marvin.buss@gmail.com>
2023-02-23 14:36:19 +0100

    key vault deployment

--- code/data-landing-zone/modules/databricks/outputs.tf
+++ code/data-landing-zone/modules/databricks/outputs.tf
@@ -15,3 +15,15 @@ output "databricks_workspace_id" {
   description = "Specifies the ID of the Databricks workspace."
   sensitive   = false
 }
+
+output "key_vault_id" {
+  value = azurerm_key_vault.key_vault.id
+  description = "Specifies the resource ID of the Key Vault."
+  sensitive   = false
+}
+
+output "key_vault_uri" {
+  value = azurerm_key_vault.key_vault.vault_uri
+  description = "Specifies the uri of the Key Vault."
+  sensitive   = false
+}
