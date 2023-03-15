e03bef304b0cfd283be075afa24381767f449163
Marvin Buss <marvin.buss@gmail.com>
2023-03-15 13:25:44 +0100

    Add enriched datalake

--- code/data-landing-zone/datalakes.tf
+++ code/data-landing-zone/datalakes.tf
@@ -11,6 +11,19 @@ module "datalake_raw" {
   private_dns_zone_id_dfs   = var.private_dns_zone_id_dfs
 }
 
+module "datalake_enriched" {
+  source = "./modules/datalake"
+
+  location                  = var.location
+  resource_group_name       = azurerm_resource_group.storage_rg.name
+  tags                      = var.tags
+  datalake_name             = replace("-", "${local.prefix}-st-enr")
+  datalake_filesystem_names = ["data"]
+  subnet_id                 = azurerm_subnet.storage_subnet.id
+  private_dns_zone_id_blob  = var.private_dns_zone_id_blob
+  private_dns_zone_id_dfs   = var.private_dns_zone_id_dfs
+}
+
 module "datalake_curated" {
   source = "./modules/datalake"
 
