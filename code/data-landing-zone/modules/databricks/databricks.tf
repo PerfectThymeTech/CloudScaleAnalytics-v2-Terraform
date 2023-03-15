d021915d11ceff2dc8a47119060bcb0988768283
Marvin Buss <marvin.buss@gmail.com>
2023-03-15 19:36:36 +0100

    Fix reference to rg group name

--- code/data-landing-zone/modules/databricks/databricks.tf
+++ code/data-landing-zone/modules/databricks/databricks.tf
@@ -1,7 +1,7 @@
 resource "azurerm_databricks_workspace" "databricks" {
   name                = var.workspace_name
   location            = var.location
-  resource_group_name = azurerm_resource_group.runtimes_rg
+  resource_group_name = var.resource_group_name
   tags                = var.tags
 
   infrastructure_encryption_enabled     = true
