322ee063781eea44461cd1f2f9a307c9e81d0efd
Marvin Buss <marvin.buss@gmail.com>
2023-03-24 17:04:18 +0100

    Add ADB role assignment to key vault

--- code/data-landing-zone/modules/databricks/roleassignments.tf
+++ code/data-landing-zone/modules/databricks/roleassignments.tf
@@ -3,3 +3,9 @@ resource "azurerm_role_assignment" "client_roleassignment_key_vault" {
   role_definition_name = "Key Vault Secrets Officer"
   principal_id         = data.azurerm_client_config.current.client_id
 }
+
+resource "azurerm_role_assignment" "databricks_roleassignment_key_vault" {
+  scope                = azurerm_key_vault.key_vault.id
+  role_definition_name = "Key Vault Secrets User"
+  principal_id         = local.databricks.enterprise_application_id
+}
