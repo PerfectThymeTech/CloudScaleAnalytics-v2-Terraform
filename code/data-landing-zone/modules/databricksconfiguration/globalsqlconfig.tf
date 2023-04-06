0bab873d1a455034a26da3ed4d5fdf615f3ad45f
Marvin Buss <marvin.buss@gmail.com>
2023-04-06 16:38:32 +0200

    Remove deprecated property

--- code/data-landing-zone/modules/databricksconfiguration/globalsqlconfig.tf
+++ code/data-landing-zone/modules/databricksconfiguration/globalsqlconfig.tf
@@ -9,7 +9,6 @@ resource "databricks_sql_global_config" "databricks_global_sql_config" {
     "spark.hadoop.fs.azure.account.oauth2.client.secret" : "{{secrets/${databricks_secret_scope.platform_secret_scope.name}/${var.client_secret_secret_name}}}",
     "spark.hadoop.fs.azure.account.oauth2.client.endpoint" : "https://login.microsoftonline.com/${data.azurerm_client_config.current.tenant_id}/oauth2/token"
   }
-  enable_serverless_compute = true
   sql_config_params = {
     "ANSI_MODE"                 = "TRUE",
     "LEGACY_TIME_PARSER_POLICY" = "CORRECTED",
