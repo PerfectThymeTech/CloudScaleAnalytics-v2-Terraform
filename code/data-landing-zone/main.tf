f2b9a8ba9ad1e119e3e88ce1062c9d5fdc6831a7
Marvin Buss <marvin.buss@gmail.com>
2023-04-08 01:24:30 +0200

    update provider config

--- code/data-landing-zone/main.tf
+++ code/data-landing-zone/main.tf
@@ -67,16 +67,6 @@ provider "azurerm" {
     resource_group {
       prevent_deletion_if_contains_resources = true
     }
-    virtual_machine {
-      delete_os_disk_on_deletion     = true
-      graceful_shutdown              = false
-      skip_shutdown_and_force_delete = false
-    }
-    virtual_machine_scale_set {
-      force_delete                  = false
-      roll_instances_when_required  = true
-      scale_to_zero_before_deletion = true
-    }
   }
 }
 
