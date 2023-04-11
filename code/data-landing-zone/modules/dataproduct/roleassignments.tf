b285dbfe239c5e7835f17bd1b709cdb70e905aef
Marvin Buss <marvin.buss@gmail.com>
2023-04-11 23:59:50 +0200

    Update references and conditions

--- code/data-landing-zone/modules/dataproduct/roleassignments.tf
+++ code/data-landing-zone/modules/dataproduct/roleassignments.tf
@@ -12,19 +12,22 @@ resource "azurerm_role_assignment" "user_assigned_identity_roleassignment_contai
 }
 
 resource "azurerm_role_assignment" "user_assigned_identity_roleassignment_container_enriched" {
-  scope                = azapi_resource.container_enriched[0].id
+  count                = var.containers_enabled.enriched && var.user_assigned_identity_enabled ? 1 : 0
+  scope                = one(azapi_resource.container_enriched[*].id)
   role_definition_name = "Storage Blob Data Contributor"
-  principal_id         = azurerm_user_assigned_identity.user_assigned_identity[0].principal_id
+  principal_id         = one(azurerm_user_assigned_identity.user_assigned_identity[*].principal_id)
 }
 
 resource "azurerm_role_assignment" "user_assigned_identity_roleassignment_container_curated" {
-  scope                = azapi_resource.container_curated[0].id
+  count                = var.containers_enabled.curated && var.user_assigned_identity_enabled ? 1 : 0
+  scope                = one(azapi_resource.container_curated[*].id)
   role_definition_name = "Storage Blob Data Contributor"
-  principal_id         = azurerm_user_assigned_identity.user_assigned_identity[0].principal_id
+  principal_id         = one(azurerm_user_assigned_identity.user_assigned_identity[*].principal_id)
 }
 
 resource "azurerm_role_assignment" "user_assigned_identity_roleassignment_container_workspace" {
-  scope                = azapi_resource.container_workspace[0].id
+  count                = var.containers_enabled.workspace && var.user_assigned_identity_enabled ? 1 : 0
+  scope                = one(azapi_resource.container_workspace[*].id)
   role_definition_name = "Storage Blob Data Contributor"
-  principal_id         = azurerm_user_assigned_identity.user_assigned_identity[0].principal_id
+  principal_id         = one(azurerm_user_assigned_identity.user_assigned_identity[*].principal_id)
 }
