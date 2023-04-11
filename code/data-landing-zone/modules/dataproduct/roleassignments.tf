a1553708007a66ee1d1ecb1d01c4fa8289a1a080
Marvin Buss <marvin.buss@gmail.com>
2023-04-11 20:50:39 +0200

    Update index for role assignments

--- code/data-landing-zone/modules/dataproduct/roleassignments.tf
+++ code/data-landing-zone/modules/dataproduct/roleassignments.tf
@@ -1,29 +1,29 @@
 resource "azurerm_role_assignment" "user_assigned_identity_roleassignment_subnet" {
   scope                = azapi_resource.subnet.id
   role_definition_name = "Network Contributor"
-  principal_id         = azurerm_user_assigned_identity.user_assigned_identity.principal_id
+  principal_id         = azurerm_user_assigned_identity.user_assigned_identity[0].principal_id
 }
 
 resource "azurerm_role_assignment" "user_assigned_identity_roleassignment_container_raw" {
-  scope                = azapi_resource.container_raw.id
+  scope                = azapi_resource.container_raw[0].id
   role_definition_name = "Storage Blob Data Contributor"
-  principal_id         = azurerm_user_assigned_identity.user_assigned_identity.principal_id
+  principal_id         = azurerm_user_assigned_identity.user_assigned_identity[0].principal_id
 }
 
 resource "azurerm_role_assignment" "user_assigned_identity_roleassignment_container_enriched" {
-  scope                = azapi_resource.container_enriched.id
+  scope                = azapi_resource.container_enriched[0].id
   role_definition_name = "Storage Blob Data Contributor"
-  principal_id         = azurerm_user_assigned_identity.user_assigned_identity.principal_id
+  principal_id         = azurerm_user_assigned_identity.user_assigned_identity[0].principal_id
 }
 
 resource "azurerm_role_assignment" "user_assigned_identity_roleassignment_container_curated" {
-  scope                = azapi_resource.container_curated.id
+  scope                = azapi_resource.container_curated[0].id
   role_definition_name = "Storage Blob Data Contributor"
-  principal_id         = azurerm_user_assigned_identity.user_assigned_identity.principal_id
+  principal_id         = azurerm_user_assigned_identity.user_assigned_identity[0].principal_id
 }
 
 resource "azurerm_role_assignment" "user_assigned_identity_roleassignment_container_workspace" {
-  scope                = azapi_resource.container_workspace.id
+  scope                = azapi_resource.container_workspace[0].id
   role_definition_name = "Storage Blob Data Contributor"
-  principal_id         = azurerm_user_assigned_identity.user_assigned_identity.principal_id
+  principal_id         = azurerm_user_assigned_identity.user_assigned_identity[0].principal_id
 }
