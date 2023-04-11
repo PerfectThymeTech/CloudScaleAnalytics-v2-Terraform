59c8dd1f04f3b6f4d8cf95b9ce1cbd8862c26284
Marvin Buss <marvin.buss@gmail.com>
2023-04-11 23:54:51 +0200

    test deployment for raw container

--- code/data-landing-zone/modules/dataproduct/roleassignments.tf
+++ code/data-landing-zone/modules/dataproduct/roleassignments.tf
@@ -5,14 +5,14 @@ resource "azurerm_role_assignment" "user_assigned_identity_roleassignment_subnet
 }
 
 resource "azurerm_role_assignment" "user_assigned_identity_roleassignment_container_raw" {
+  count                = var.containers_enabled.raw && var.user_assigned_identity_enabled ? 1 : 0
   scope                = one(azapi_resource.container_raw[*].id)
   role_definition_name = "Storage Blob Data Contributor"
-  principal_id         = azurerm_user_assigned_identity.user_assigned_identity[0].principal_id
+  principal_id         = one(azurerm_user_assigned_identity.user_assigned_identity[*].principal_id)
 }
 
 resource "azurerm_role_assignment" "user_assigned_identity_roleassignment_container_enriched" {
-  for_each             = azapi_resource.container_enriched
-  scope                = azapi_resource.container_enriched[each.key].id
+  scope                = azapi_resource.container_enriched[0].id
   role_definition_name = "Storage Blob Data Contributor"
   principal_id         = azurerm_user_assigned_identity.user_assigned_identity[0].principal_id
 }
