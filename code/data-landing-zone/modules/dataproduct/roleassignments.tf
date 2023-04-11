e93abc9bc5ada1757f20562764aa0cf564880d56
Marvin Buss <marvin.buss@gmail.com>
2023-04-11 23:37:21 +0200

    test

--- code/data-landing-zone/modules/dataproduct/roleassignments.tf
+++ code/data-landing-zone/modules/dataproduct/roleassignments.tf
@@ -5,7 +5,7 @@ resource "azurerm_role_assignment" "user_assigned_identity_roleassignment_subnet
 }
 
 resource "azurerm_role_assignment" "user_assigned_identity_roleassignment_container_raw" {
-  scope                = azapi_resource.container_raw[0].id
+  scope                = one(azapi_resource.container_raw[*].id)
   role_definition_name = "Storage Blob Data Contributor"
   principal_id         = azurerm_user_assigned_identity.user_assigned_identity[0].principal_id
 }
