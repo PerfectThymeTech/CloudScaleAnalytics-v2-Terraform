445a8ceeaa16133fc906e4f6bee1ee1faff5db4b
Marvin Buss <marvin.buss@gmail.com>
2023-04-12 17:35:31 +0200

    Update conditions

--- code/data-landing-zone/modules/dataproduct/roleassignments.tf
+++ code/data-landing-zone/modules/dataproduct/roleassignments.tf
@@ -6,35 +6,35 @@ resource "azurerm_role_assignment" "user_assigned_identity_roleassignment_resour
 }
 
 resource "azurerm_role_assignment" "user_assigned_identity_roleassignment_subnet" {
-  count                = one(azapi_resource.subnet[*].id) != null && one(data.azuread_group.security_group[*].object_id) != null ? 1 : 0
+  count                = var.network_enabled && one(data.azuread_group.security_group[*].object_id) != null ? 1 : 0
   scope                = one(azapi_resource.subnet[*].id)
   role_definition_name = "Network Contributor"
   principal_id         = one(data.azuread_group.security_group[*].object_id)
 }
 
 resource "azurerm_role_assignment" "user_assigned_identity_roleassignment_container_raw" {
-  count                = one(azapi_resource.container_raw[*].id) != null && one(data.azuread_group.security_group[*].object_id) != null ? 1 : 0
+  count                = var.containers_enabled.raw != null && one(data.azuread_group.security_group[*].object_id) != null ? 1 : 0
   scope                = one(azapi_resource.container_raw[*].id)
   role_definition_name = "Storage Blob Data Contributor"
   principal_id         = one(data.azuread_group.security_group[*].object_id)
 }
 
 resource "azurerm_role_assignment" "user_assigned_identity_roleassignment_container_enriched" {
-  count                = one(azapi_resource.container_enriched[*].id) != null && one(data.azuread_group.security_group[*].object_id) != null ? 1 : 0
+  count                = var.containers_enabled.enriched != null && one(data.azuread_group.security_group[*].object_id) != null ? 1 : 0
   scope                = one(azapi_resource.container_enriched[*].id)
   role_definition_name = "Storage Blob Data Contributor"
   principal_id         = one(data.azuread_group.security_group[*].object_id)
 }
 
 resource "azurerm_role_assignment" "user_assigned_identity_roleassignment_container_curated" {
-  count                = one(azapi_resource.container_curated[*].id) != null && one(data.azuread_group.security_group[*].object_id) != null ? 1 : 0
+  count                = var.containers_enabled.curated != null && one(data.azuread_group.security_group[*].object_id) != null ? 1 : 0
   scope                = one(azapi_resource.container_curated[*].id)
   role_definition_name = "Storage Blob Data Contributor"
   principal_id         = one(data.azuread_group.security_group[*].object_id)
 }
 
 resource "azurerm_role_assignment" "user_assigned_identity_roleassignment_container_workspace" {
-  count                = one(azapi_resource.container_workspace[*].id) != null && one(data.azuread_group.security_group[*].object_id) != null ? 1 : 0
+  count                = var.containers_enabled.workspace != null && one(data.azuread_group.security_group[*].object_id) != null ? 1 : 0
   scope                = one(azapi_resource.container_workspace[*].id)
   role_definition_name = "Storage Blob Data Contributor"
   principal_id         = one(data.azuread_group.security_group[*].object_id)
