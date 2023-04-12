89ce5f0088b775e9b4881978971bbd74dddc2757
Marvin Buss <marvin.buss@gmail.com>
2023-04-13 01:07:24 +0200

    fix subnet reference

--- code/data-landing-zone/modules/dataproduct/keyvault.tf
+++ code/data-landing-zone/modules/dataproduct/keyvault.tf
@@ -140,7 +140,7 @@ resource "azurerm_private_endpoint" "key_vault_private_endpoint" {
     private_connection_resource_id = azurerm_key_vault.key_vault.id
     subresource_names              = ["vault"]
   }
-  subnet_id = azapi_resource.subnet.id
+  subnet_id = one(azapi_resource.subnet[*].id)
   dynamic "private_dns_zone_group" {
     for_each = var.private_dns_zone_id_key_vault == "" ? [] : [1]
     content {
