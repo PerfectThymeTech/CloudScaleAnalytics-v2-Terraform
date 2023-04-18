bfc24c2cce8d8bc257bee3ba2fc7449285f698b5
Marvin Buss <marvin.buss@gmail.com>
2023-04-18 20:12:58 +0200

    Add locals for adb setup

--- code/data-landing-zone/modules/dataproduct/locals.tf
+++ code/data-landing-zone/modules/dataproduct/locals.tf
@@ -1,14 +1,19 @@
 locals {
   names = {
-    resource_group         = "${var.data_product_name}-rg"
-    subnet                 = "DataProductSubnet-${var.data_product_name}"
-    user_assigned_identity = "${var.data_product_name}-uai001"
-    key_vault              = "${var.data_product_name}-vault001"
-    container_raw          = var.data_product_name
-    container_enriched     = var.data_product_name
-    container_curated      = var.data_product_name
-    container_workspace    = var.data_product_name
-    service_principal      = var.data_product_name
+    resource_group              = "${var.data_product_name}-rg"
+    subnet                      = "DataProductSubnet-${var.data_product_name}"
+    user_assigned_identity      = "${var.data_product_name}-uai001"
+    key_vault                   = "${var.data_product_name}-vault001"
+    container_raw               = var.data_product_name
+    container_enriched          = var.data_product_name
+    container_curated           = var.data_product_name
+    container_workspace         = var.data_product_name
+    service_principal           = var.data_product_name
+    databricks_access_connector = "${var.data_product_name}-dbac001"
+    databricks_cluster          = "${var.data_product_name}-cluster001"
+    databricks_storage_credential = var.data_product_name
+    databricks_external_location = var.data_product_name
+    databricks_catalog          = var.data_product_name
   }
 
   virtual_network = {
@@ -45,4 +50,7 @@ locals {
     resource_group_name = try(split("/", var.datalake_workspace_id)[4], "")
     name                = try(split("/", var.datalake_workspace_id)[8], "")
   }
+
+  databricks_catalog_storage_root_container = ""
+  databricks_catalog_storage_root = "abfss://${data.azurerm_storage_account.datalake_workspace.name}@${local.databricks_catalog_storage_root_container}.dfs.core.windows.net/"
 }
