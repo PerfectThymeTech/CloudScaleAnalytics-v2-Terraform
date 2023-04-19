d056e1902460381fa6aae80fb2f5ab2ab553fc0c
Marvin Buss <marvin.buss@gmail.com>
2023-04-19 12:26:08 +0200

    lint

--- code/data-landing-zone/modules/dataproduct/databricksunity_experimentation.tf
+++ code/data-landing-zone/modules/dataproduct/databricksunity_experimentation.tf
@@ -1,7 +1,7 @@
 resource "databricks_storage_credential" "experimentation_storage_credential" {
-  count        = var.databricks_enabled && var.databricks_experimentation && var.unity_catalog_configurations.enabled ? 1 : 0
-  provider     = databricks.experimentation
-  name         = local.names.databricks_storage_credential
+  count    = var.databricks_enabled && var.databricks_experimentation && var.unity_catalog_configurations.enabled ? 1 : 0
+  provider = databricks.experimentation
+  name     = local.names.databricks_storage_credential
 
   azure_managed_identity {
     access_connector_id = one(azurerm_databricks_access_connector.databricks_access_connector[*].id)
