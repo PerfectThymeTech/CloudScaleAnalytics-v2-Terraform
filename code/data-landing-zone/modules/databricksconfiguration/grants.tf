4e2ab60a92b62b06c8b27dbf14300b8ffd5af537
Marvin Buss <marvin.buss@gmail.com>
2023-04-19 15:04:00 +0200

    Update grants

--- code/data-landing-zone/modules/databricksconfiguration/grants.tf
+++ code/data-landing-zone/modules/databricksconfiguration/grants.tf
@@ -1,9 +1,9 @@
-data "databricks_current_user" "current" {}
+data "databricks_current_user" "current_user" {}
 
-resource "databricks_grants" "sandbox" {
-  catalog = var.unity_metastore_id
+resource "databricks_grants" "grants_current_user" {
+  catalog = var.unity_metastore_name
   grant {
-    principal = data.databricks_current_user.current.user_name
+    principal = data.databricks_current_user.current_user.user_name
     privileges = [
       "CREATE_CATALOG",
       "CREATE_EXTERNAL_LOCATION"
