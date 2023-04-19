1890923f36d9bb07df9fc93dc3579e20e6d8dbe5
Marvin Buss <marvin.buss@gmail.com>
2023-04-19 15:17:10 +0200

    Add databricks grants

--- code/data-landing-zone/modules/databricksconfiguration/grants.tf
+++ code/data-landing-zone/modules/databricksconfiguration/grants.tf
@@ -1,7 +1,7 @@
 data "databricks_current_user" "current_user" {}
 
 resource "databricks_grants" "grants_current_user" {
-  catalog = var.unity_metastore_name
+  metastore = var.unity_metastore_id
   grant {
     principal = data.databricks_current_user.current_user.user_name
     privileges = [
