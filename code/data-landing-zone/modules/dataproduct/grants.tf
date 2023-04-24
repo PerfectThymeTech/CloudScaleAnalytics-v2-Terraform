52b5e9416d7e65f794872635a69c03f5c0f3abfe
Marvin Buss <marvin.buss@gmail.com>
2023-04-24 17:19:30 +0200

    Clean up code

--- code/data-landing-zone/modules/dataproduct/grants.tf
+++ code/data-landing-zone/modules/dataproduct/grants.tf
@@ -1,8 +1,3 @@
-# data "databricks_group" "security_group" {
-#   count        = var.databricks_enabled && var.unity_catalog_configurations.enabled && var.unity_catalog_configurations.group_name != "" ? 1 : 0
-#   display_name = var.unity_catalog_configurations.group_name
-# }
-
 resource "databricks_grants" "grants_experimentation_catalog" {
   count   = var.databricks_enabled && var.unity_catalog_configurations.enabled && var.databricks_experimentation && var.unity_catalog_configurations.group_name != "" ? 1 : 0
   catalog = one(databricks_catalog.experimentation_catalog[*].name)
