336f964ba46dcee615d55dc2db53b15208432e43
Marvin Buss <marvin.buss@gmail.com>
2023-04-24 16:35:40 +0200

    Test grant to group

--- code/data-landing-zone/modules/dataproduct/grants.tf
+++ code/data-landing-zone/modules/dataproduct/grants.tf
@@ -1,22 +1,22 @@
-data "databricks_group" "security_group" {
-  count        = var.databricks_enabled && var.unity_catalog_configurations.enabled && var.unity_catalog_configurations.group_name != "" ? 1 : 0
-  display_name = var.unity_catalog_configurations.group_name
-}
+# data "databricks_group" "security_group" {
+#   count        = var.databricks_enabled && var.unity_catalog_configurations.enabled && var.unity_catalog_configurations.group_name != "" ? 1 : 0
+#   display_name = var.unity_catalog_configurations.group_name
+# }
 
 resource "databricks_grants" "grants_experimentation_catalog" {
-  count   = var.databricks_experimentation && one(data.databricks_group.security_group[*].id) != null ? 1 : 0
+  count   = var.databricks_enabled && var.unity_catalog_configurations.enabled && var.databricks_experimentation && var.unity_catalog_configurations.group_name != "" ? 1 : 0
   catalog = one(databricks_catalog.experimentation_catalog[*].name)
   grant {
-    principal  = one(data.databricks_group.security_group[*].id)
+    principal  = var.unity_catalog_configurations.group_name
     privileges = ["ALL_PRIVILEGES"]
   }
 }
 
 resource "databricks_grants" "grants_automation_catalog" {
-  count   = !var.databricks_experimentation && one(data.databricks_group.security_group[*].id) != null ? 1 : 0
+  count   = var.databricks_enabled && var.unity_catalog_configurations.enabled && !var.databricks_experimentation && var.unity_catalog_configurations.group_name != "" ? 1 : 0
   catalog = one(databricks_catalog.automation_catalog[*].name)
   grant {
-    principal  = one(data.databricks_group.security_group[*].id)
+    principal  = var.unity_catalog_configurations.group_name
     privileges = ["ALL_PRIVILEGES"]
   }
 }
