8b8bcdc9e5b1b4fe31190be4b095e3bcd3fd747d
Marvin Buss <marvin.buss@gmail.com>
2023-04-26 14:51:29 +0200

    lint

--- code/data-management-zone/network.tf
+++ code/data-management-zone/network.tf
@@ -66,7 +66,7 @@ resource "azapi_resource" "databricks_private_subnet" {
       serviceEndpoints        = []
     }
   })
-  
+
   depends_on = [
     azapi_resource.private_endpoint_subnet
   ]
@@ -101,7 +101,7 @@ resource "azapi_resource" "databricks_public_subnet" {
       serviceEndpoints        = []
     }
   })
-  
+
   depends_on = [
     azapi_resource.databricks_private_subnet
   ]
