cf61b2b5b269b5cb420094cbaf99695d2eaaf00c
Marvin Buss <marvin.buss@gmail.com>
2023-04-26 22:58:39 +0200

    Comment out grants

--- code/data-landing-zone/modules/databricksconfiguration/grants.tf
+++ code/data-landing-zone/modules/databricksconfiguration/grants.tf
@@ -1,12 +1,12 @@
-data "databricks_current_user" "current_user" {}
+# data "databricks_current_user" "current_user" {}
 
-resource "databricks_grants" "grants_current_user" {
-  metastore = var.unity_metastore_id
-  grant {
-    principal = data.databricks_current_user.current_user.user_name
-    privileges = [
-      "CREATE_CATALOG",
-      "CREATE_EXTERNAL_LOCATION"
-    ]
-  }
-}
+# resource "databricks_grants" "grants_current_user" {
+#   metastore = var.unity_metastore_id
+#   grant {
+#     principal = data.databricks_current_user.current_user.user_name
+#     privileges = [
+#       "CREATE_CATALOG",
+#       "CREATE_EXTERNAL_LOCATION"
+#     ]
+#   }
+# }
