64c74c5a89476f9e2d8d4a1fc4a6a97639ce4ec2
Marvin Buss <marvin.buss@gmail.com>
2023-04-26 15:14:15 +0200

    Update auth

--- code/data-management-zone/main.tf
+++ code/data-management-zone/main.tf
@@ -64,8 +64,8 @@ provider "azapi" {
 
 provider "databricks" {
   azure_environment           = "public"
-  azure_workspace_resource_id = azurerm_databricks_workspace.databricks.id
-  host                        = azurerm_databricks_workspace.databricks.workspace_url
+  # azure_workspace_resource_id = azurerm_databricks_workspace.databricks.id
+  host                        = "https://accounts.azuredatabricks.net"
 }
 
 data "azurerm_client_config" "current" {
