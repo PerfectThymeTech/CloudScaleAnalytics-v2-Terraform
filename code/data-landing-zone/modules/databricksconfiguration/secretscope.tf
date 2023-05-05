bfa4224d3a0fd3de1709132801d60535c4cb2369
Marvin Buss <marvin.buss@gmail.com>
2023-05-05 14:44:53 +0200

    Add group assignment

--- code/data-landing-zone/modules/databricksconfiguration/secretscope.tf
+++ code/data-landing-zone/modules/databricksconfiguration/secretscope.tf
@@ -8,9 +8,21 @@ resource "databricks_secret_scope" "platform_secret_scope" {
   }
 }
 
+data "databricks_current_user" "current" {}
+
 data "databricks_group" "group" {
   count        = var.databricks_admin_groupname != "" ? 1 : 0
   display_name = var.databricks_admin_groupname
+
+  provider = databricks.account
+}
+
+resource "databricks_mws_permission_assignment" "permission_assignment" {
+  workspace_id = var.databricks_workspace_id
+  principal_id = one(data.databricks_group.group[*].id)
+  permissions = [
+    "ADMIN"
+  ]
 
   provider = databricks.account
 }
@@ -20,4 +32,8 @@ resource "databricks_secret_acl" "secret_acl" {
   principal  = one(data.databricks_group.group[*].display_name)
   permission = "MANAGE"
   scope      = databricks_secret_scope.platform_secret_scope.name
+
+  depends_on = [
+    databricks_mws_permission_assignment.permission_assignment
+  ]
 }
